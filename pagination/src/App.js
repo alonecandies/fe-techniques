7c49396db9cb8db855be7824ccc85dc25eed9c4a
alonecandies <dragonfrezze@gmail.com>
2022-07-05 14:17:57 +0700

    pagination

---
+++ pagination/src/App.js
@@ -0,0 +1,67 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import ReactPaginate from "react-paginate";
+
+function App() {
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const items = useMemo(() => {
+    return [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+      40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+      58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
+      76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93,
+      94, 95, 96, 97, 98, 99, 100,
+    ];
+  }, []);
+
+  const itemsPerPage = 8;
+
+  const Items = useCallback(({ items }) => {
+    return items?.map((item, index) => {
+      return <div key={index}>Item #{item}</div>;
+    });
+  }, []);
+
+  const handlePageClick = useCallback(
+    (event) => {
+      const newOffset = (event.selected * itemsPerPage) % items.length;
+      console.log(
+        `User requested page number ${event.selected}, which is offset ${newOffset}`
+      );
+      setItemOffset(newOffset);
+    },
+    [items]
+  );
+
+  useEffect(() => {
+    // Fetch items from another resources.
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    setCurrentItems(items.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(items.length / itemsPerPage));
+  }, [itemOffset, items, itemsPerPage]);
+
+  return (
+    <div className="App">
+      <Items items={currentItems} />
+      <ReactPaginate
+        previousLabel="<"
+        nextLabel=">"
+        breakLabel="..."
+        breakClassName="break-me"
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        activeClassName={"active"}
+        renderOnZeroPageCount={null}
+      />
+    </div>
+  );
+}
+
+export default App;
